be6959944a3ecb3b23f5a7f3904cad3c19be3eb2
lhh <lanhanghui@gmail.com>
2023-07-27 14:08:12 +0800

    feat: img fade-in

--- textFadeIn.js
+++ textFadeIn.js
@@ -1,10 +1,24 @@
 const textFadeInIds = ["service_scope", "service_scope_desc"];
 
-function handleIntersection(entries) {
+function handleTextIntersection(entries) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // 当目标元素进入视口时执行的操作
+      // 当目标元素进入视口时执行的 slide-in-left
       entry.target.classList.add("slide-in-left");
+    }
+  });
+}
+
+function handleImgIntersection(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // 当目标元素进入视口时执行的 slide-in
+      entry.target.setAttribute("data-into-view", "true");
+
+      const isLoad = entry.target.getAttribute("data-load") === "true";
+      if (isLoad) {
+        entry.target.classList.add("fade-in");
+      }
     }
   });
 }
@@ -14,10 +28,19 @@ function initTextFadeIn() {
     const id = textFadeInIds[i];
     const element = document.getElementById(id);
     if (element) {
-      const observer = new IntersectionObserver(handleIntersection);
+      const observer = new IntersectionObserver(handleTextIntersection);
       observer.observe(element);
     }
   }
 }
 
+function initImgFadeIn() {
+  const imageList = document.querySelectorAll(".service-img");
+  for (let i = 0; i < imageList.length; i++) {
+    const observer = new IntersectionObserver(handleImgIntersection);
+    observer.observe(imageList[i]);
+  }
+}
+
 initTextFadeIn();
+initImgFadeIn();

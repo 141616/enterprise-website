c832a10cade70112f0fa7f109b26db6231bb89c1
lhh <lanhanghui@gmail.com>
2023-07-27 12:10:01 +0800

    fix: 优化滚动

--- menu.js
+++ menu.js
@@ -5,11 +5,19 @@ function handleClickMask() {
   closeDrawer();
 }
 
+function getSrcollBarWidth() {
+  return window.innerWidth - document.body.clientWidth;
+}
+
 function openDrawer() {
+  // hidden之后滚动条消失，页面宽度变化会导致布局变化，这里添加一个padding保证页面布局不变
+  const scrollBarWidth = getSrcollBarWidth();
+  if (scrollBarWidth > 0) {
+    document.body.style.paddingRight = `${scrollBarWidth}px`;
+  }
   // 阻止滚动
   document.body.style.overflow = "hidden";
-  // hidden之后滚动条消失，页面宽度变化会导致布局变化，这里添加一个padding保证页面布局不变
-  // document.body.style.paddingRight = "15px";
+
   const drawerContentEle = document.getElementById("drawer-content");
   drawerContentEle.classList.add("slide-in-right");
   const drawerEle = document.getElementById("drawer");
@@ -18,7 +26,7 @@ function openDrawer() {
 
 function closeDrawer() {
   document.body.style.overflow = "";
-  // document.body.style.paddingRight = "";
+  document.body.style.paddingRight = "";
   const drawerEle = document.getElementById("drawer");
   drawerEle.classList.add("hidden");
   const drawerContentEle = document.getElementById("drawer-content");
